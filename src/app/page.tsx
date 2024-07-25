0f25c0223a48f25cb53a9e454c02b2ff3c00c5dd
the-pro7 <ameyawemmanuel900@gmail.com>
2024-07-25 09:47:01 -0700

    Added verifications route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import styles from "./page.module.css";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Link from "next/link";
+import {Button, Container, Stack} from "react-bootstrap"
 
 export default function Home() {
   return (
-    <>
-    <h1>Hello</h1>
-    </>
+    <Container className="py-4 h-100 d-flex flex-column justify-content-center align-items-center">
+    <h1>Welcome to Superate</h1>
+    <Stack direction="horizontal" gap={2}>
+    <Button variant="primary">
+      <Link href="/verification" className={`link-underline-none ${styles.verifyLink}`}>Go to Verifications</Link>
+    </Button>
+    {/* <Button variant="outline-primary">
+      <Link href="/verification" className="text-no-underline">Go to Verifications</Link>
+    </Button> */}
+    </Stack>
+    </Container>
   );
 }
