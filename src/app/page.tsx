083131d51e937549a3108c0c07fd3c1d7e1a85d5
the-pro7 <ameyawemmanuel900@gmail.com>
2024-07-28 21:14:21 +0000

    fixed another minor issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,25 @@ import LoginForm from "@/components/form/LoginForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 export default async function Home() {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useState<string | null>("");
 
-  const dialogRef = useRef<HTMLDialogElement>(null)
-  const router = useRouter()
-
-  const accessToken = localStorage.getItem("accessToken")
+  useEffect(() => {
+    if (typeof window != "undefined") {
+      setAccessToken(localStorage.getItem("accessToken"));
+    }
+  }, []);
 
   function openDialog() {
-    dialogRef.current?.showModal()
+    dialogRef.current?.showModal();
   }
 
   function closeDialog() {
-    dialogRef.current?.close()
+    dialogRef.current?.close();
   }
 
   return (
@@ -32,20 +36,30 @@ export default async function Home() {
       <h1 className="font-bold text-gray-800 text-5xl mt-4">Superate App</h1>
       <div className="inline">
         {/* <Link href="/verification" className="btn rounded-xl p-4 bg-sky-700 text-white">Go to verification dashboard</Link> */}
-        <button className="btn bg-sky-500 p-4 rounded-lg text-white tracking-widest" type="button" onClick={() => {
-          if (accessToken) {
-            router.push("/verification")
-          }
+        <button
+          className="btn bg-sky-500 p-4 rounded-lg text-white tracking-widest"
+          type="button"
+          onClick={() => {
+            if (accessToken) {
+              router.push("/verification");
+            }
 
-          openDialog()
-        }}>
+            openDialog();
+          }}
+        >
           Go to verification dashboard
         </button>
       </div>
       <dialog ref={dialogRef} className="p-4 w-1/4 overflow-clip rounded-lg">
-        <Button type="button" onClick={closeDialog} className="text-xs font-light bg-gray-500 text-white hover:bg-black ml-auto">Close</Button>
+        <Button
+          type="button"
+          onClick={closeDialog}
+          className="text-xs font-light bg-gray-500 text-white hover:bg-black ml-auto"
+        >
+          Close
+        </Button>
         <h2 className="text-xl font-semibold tracking-wider">Authentication</h2>
-       <LoginForm />
+        <LoginForm />
       </dialog>
     </main>
   );
